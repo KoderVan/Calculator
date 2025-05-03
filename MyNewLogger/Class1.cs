3303a4785b324b21fde59f4b5eb5807b84a0fa9f
Ivan <vancrash00@gmail.com>
2025-05-03 19:28:45 +0500

    upd

---
+++ MyNewLogger/Class1.cs
@@ -0,0 +1,86 @@
+﻿using System.Threading.Channels;
+
+namespace MyNewLogger
+{
+    public interface ILogger
+    {
+        public void LogInformation(string message);
+        public void LogError(Exception exeption, string? additionalMessage = null);
+    }
+
+    public class ConsoleLogger : ILogger
+    {
+        public void LogInformation(string message)
+        {
+            Console.WriteLine($"LogInformation: {message}");
+        }
+
+        public void LogError(Exception exeption, string? additionalMessage = null)
+        {
+            Console.WriteLine($"LogError:{exeption}");
+            if (additionalMessage != null)
+            {
+                Console.WriteLine(additionalMessage);
+            }
+        }
+    }
+
+    public class FileLogger : ILogger
+    {
+        
+        public void LogInformation(string message)
+        {
+            using (var sw = new StreamWriter("log.txt"))
+            {
+                sw.WriteLine($"LogInformation: {message}");
+            }  
+        }
+
+        public void LogError(Exception exeption, string? additionalMessage = null)
+        {
+            using (var sw = new StreamWriter("log.txt"))
+            {
+                sw.WriteLine($"LogError:{exeption}");
+                if (additionalMessage != null)
+                {
+                    sw.WriteLine(additionalMessage);
+                }
+            }
+                
+        }   
+    }
+
+    public class CompositeLogger : ILogger
+    {
+        private readonly ILogger _ConsoleLogger;
+        private readonly ILogger _FileLogger;
+        public CompositeLogger(ILogger ConsoleLogger, ILogger FileLogger)
+        {
+            _ConsoleLogger = ConsoleLogger;
+            _FileLogger = FileLogger;
+        }
+
+        public void LogError(Exception exeption, string? additionalMessage = null)
+        {
+            using (var sw = new StreamWriter("log.txt"))
+            {
+                sw.WriteLine($"LogError:{exeption}");
+                Console.WriteLine($"LogError:{exeption}");
+                if (additionalMessage != null)
+                {
+                    Console.WriteLine(additionalMessage);
+                    sw.WriteLine(additionalMessage);
+                }
+            }
+        }
+
+        public void LogInformation(string message)
+        {
+            using (var sw = new StreamWriter("log.txt"))
+            {
+                sw.WriteLine($"LogInformation: {message}");
+                Console.WriteLine($"LogInformation: {message}");
+            }
+        }
+    }
+}
---
+++ NewCalc/Program.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.AccessControl;
+using System.Text;
+using System.Threading.Tasks;
+using MyNewLogger;
+
+namespace calc
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string? mathOperation;
+            var ConsoleLogger = new ConsoleLogger();
+            var FileLogger = new FileLogger();
+            var CompositeLogger = new CompositeLogger(ConsoleLogger, FileLogger);
+            Calc2 calc = new Calc2(ConsoleLogger);
+            var dict = new Dictionary<string, Func<double, double, double?>>
+            {
+                {"+", calc.Sum},
+                {"-", calc.Substr},
+                {"*", calc.Multiply},
+                {"/", calc.Divide},
+                {"%", calc.Percent},
+                {"^", calc.Exponent}
+            };
+            string[] resultString;
+            double? result = 0.0;
+
+            Console.WriteLine("Введите своё выражение:");
+            
+            while (true)
+            {
+                mathOperation = Console.ReadLine().Replace(" ", "");
+
+                if (mathOperation.ToLower() == "exit")
+                {
+                    break;
+                }
+                else if (mathOperation.ToLower() == "help")
+                {
+                    Console.WriteLine("Основные команды:\n" +
+                    "Help - справка\n" +
+                    "Exit - выход из приложения\n" +
+                    "\n" +
+                    "Поддерживаемы операции: +, -, *, /, %, ^");
+                }
+                else
+                {
+                    try
+                    {
+                        resultString = ParseString(mathOperation);
+                    }
+                    catch (Exception ex)
+                    {
+                        CompositeLogger.LogError(ex, "Не удалось распарсить строку");
+                        break;
+                    }
+                    try
+                    {
+                        result = calc.CountResult(resultString, dict);
+                        Console.WriteLine(result);
+                    }
+                    catch (Exception ex)
+                    {
+                        CompositeLogger.LogError(ex, "Не удалось распарсить строку");
+                    }
+                }
+            }
+        }
+
+        static string[] ParseString(string mathOperation)
+        {
+            string operand1 = "";
+            string operand2 = "";
+            string operation = "";
+            bool seekOperand1 = true;
+            
+            for (int i = 0; i < mathOperation.Length; i++)
+            {
+                if (seekOperand1)
+                {
+                    if (char.IsDigit(mathOperation[i]))
+                    {
+                        operand1 += mathOperation[i];
+                    }
+                    else
+                    {
+                        operation = mathOperation[i].ToString();
+                        seekOperand1 = false;
+                    }
+                }
+                else
+                {
+                    operand2 += mathOperation[i];
+                }
+            }
+            string[] mathOperationList = [operand1, operand2, operation];
+            return mathOperationList;
+        }
+
+    }
+
+    class Calc2
+    {
+        private readonly ILogger _logger;
+        public Calc2(ILogger logger)
+        {
+            _logger = logger;
+        }
+
+        public double? Sum(double operand1, double operand2)
+        {
+            _logger.LogInformation("Сложение");
+            return operand1 + operand2;
+        }
+        public double? Substr(double operand1, double operand2)
+        {
+            _logger.LogInformation("Вычитание");
+            return operand1 - operand2;
+        }
+        public double? Multiply(double operand1, double operand2)
+        {
+            _logger.LogInformation("Умножение");
+            return operand1 * operand2;
+        }
+        public double? Divide(double operand1, double operand2)
+        {
+            if (operand2 == 0)
+            {
+                _logger.LogError(new DivideByZeroException(), "Деление на ноль");
+                
+                return null;
+            }
+            else return operand1 / operand2;
+        }
+        
+        public double? Percent(double operand1, double operand2) 
+        {
+            _logger.LogInformation("Остаток");
+           return operand1 % operand2;
+        }
+        public double? Exponent(double operand1, double operand2)
+        {
+            _logger.LogInformation("Возведение в степень");
+            return Math.Pow(operand1, operand2);
+        }
+        public double? CountResult(string[] mathOperationList, Dictionary<string, Func<double, double, double?>> dict)
+        {
+            double operand1 = double.Parse(mathOperationList[0]);
+            double operand2 = double.Parse(mathOperationList[1]);
+            string operation = mathOperationList[2];
+
+            return dict[operation](operand1, operand2);
+        }
+    }
+
+}
